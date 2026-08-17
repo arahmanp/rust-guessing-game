c9214e2b9f3710fae3e4c516248650174dccc9e9
Andhika Rahman <andhikarhmnp@gmail.com>
2026-08-17 09:37:55 +0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,30 +3,13 @@ use std::io;
 fn main() {
     println!("Tebak angkanya!");
 
-    loop {
-        println!("Silakan masukkan tebakanmu.");
+    println!("Silakan tebak angkanya.");
 
-        let guess = loop {
-            let mut temp = String::new();
+    let mut guess = String::new();
 
-            io::stdin()
-                .read_line(&mut temp)
-                .expect("Gagal membaca baris input");
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Gagal membaca baris input");
 
-            match temp.trim().parse::<i32>() {
-                Ok(num) => break num,
-                Err(_) => {
-                    println!("Masukkan sebuah angka!");
-                    continue;
-                }
-            }
-        };
-
-        if guess == -1 {
-            println!("Permainan selesai..");
-            break;
-        }
-
-        println!("Anda menebak {}", guess);
-    }
+    println!("Kamu menebak: {}", guess);
 }
